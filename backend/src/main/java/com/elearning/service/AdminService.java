9d85f9890ed2eb7b81c5de80b63f1a1df4bd820a
Riddhiben Patel <riddhibenp@cybage.com>
2021-03-22 18:15:56 +0530

    ADMIN-Create-and-update-with-total-count

--- backend/src/main/java/com/elearning/service/AdminService.java
+++ backend/src/main/java/com/elearning/service/AdminService.java
@@ -5,6 +5,26 @@ import java.util.Optional;
 
 
 public interface AdminService {
+   
+
+	public boolean addCategory(Category c);
+
+	public boolean updateCategory(Category c, Optional<Category> ctest, int id);
+
+	public boolean addCourse(Course c, int id);
+
+	public boolean updateCourse(Course c, Optional<Course> ctest);
+
+	public boolean addVideo(Video v, int id);
+
+	boolean updateVideo(Video v, int id);
+
+	public long getCategoryCount();
+
+	public long getCourseCount();
+
+	public long getVideoCount();
+
 
 	
 }
--- backend/src/main/java/com/elearning/service/AdminServiceImpl.java
+++ backend/src/main/java/com/elearning/service/AdminServiceImpl.java
@@ -11,6 +11,92 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AdminServiceImpl implements AdminService{
+	@Autowired
+	CategoryRepository cat;
+	@Autowired
+	CourseRepository cou;
+	@Autowired
+	VideoRepository vr;
+
+	@Autowired
+	UserRepo ur;
+
+
+
+	@Override
+	public boolean addCategory(Category c) {
+		return cat.save(c) != null;
+	}
+
+	
+
+	@Override
+	public boolean updateCategory(Category c, Optional<Category> ctest, int id) {
+		List<Course> cotest = ctest.get().getCourses();
+		c.setCourses(cotest);
+		return cat.save(c) != null;
+	}
+
+
+
+	@Override
+	public boolean addCourse(Course c, int id) {
+		Optional<Category> cate = cat.findById(id);
+		List<Course> courses = cate.get().getCourses();
+		courses.add(c);
+		cate.get().setCourses(courses);
+		cat.save(cate.get());
+		return true;
+	}
+
+	
+	@Override
+	public boolean updateCourse(Course c, Optional<Course> ctest) {
+		List<Video> video = ctest.get().getVideo();
+		String cat_name = ctest.get().getCategory();
+		Category category = cat.findByCategoryName(cat_name);
+		c.setCategory(category);
+		c.setVideo(video);
+		return cou.save(c) != null;
+	}
+
+	@Override
+	public boolean addVideo(Video v, int id) {
+		Optional<Course> co = cou.findById(id);
+		List<Video> videos = co.get().getVideo();
+		videos.add(v);
+		co.get().setVideo(videos);
+		cou.save(co.get());
+		return true;
+	}
+
+
+	
+	@Override
+	public boolean updateVideo(Video v, int id) {
+		Optional<Video> video = vr.findById(v.getVideoId());
+		Course course = cou.findByCourseName(video.get().getCourse());
+		v.setCourse(course);
+		vr.save(v);
+		return true;
+	}
+
+
+	@Override
+	public long getCategoryCount() {
+		return cat.count();
+	}
+
+	@Override
+	public long getCourseCount() {
+		return cou.count();
+	}
+
+	@Override
+	public long getVideoCount() {
+		return cou.count();
+	}
+
 	
 	
 
