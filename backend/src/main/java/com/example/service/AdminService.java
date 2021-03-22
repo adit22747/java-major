40f223c7540aba9b377ddf01efaedc2b0ac86c22
Hemankshi Ishwar <hemankshii@cybage.com>
2021-03-22 18:25:18 +0530

    Admin-read-and-delete

--- backend/src/main/java/com/example/service/AdminService.java
+++ backend/src/main/java/com/example/service/AdminService.java
@@ -3,8 +3,24 @@ package com.example.service;
 import java.util.List;
 import java.util.Optional;
 
-
 public interface AdminService {
 
-	
+	public List<Category> getAllCategory();
+
+	public Optional<Category> getCategoryById(int id);
+
+	public void deleteCategory(int id);
+
+	public List<Course> getAllCourse();
+
+	public Optional<Course> getCourseById(int id);
+
+	public void deleteCourse(int i);
+
+	public List<Video> getAllVideo();
+
+	public Optional<Video> getVideoById(int id);
+
+	public void deleteVideo(int i);
+
 }
--- backend/src/main/java/com/example/service/AdminServiceImpl.java
+++ backend/src/main/java/com/example/service/AdminServiceImpl.java
@@ -7,12 +7,65 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
-
 @Service
-public class AdminServiceImpl implements AdminService{
-	
-	
+public class AdminServiceImpl implements AdminService {
 
+	@Autowired
+	CategoryRepository cat;
+	@Autowired
+	CourseRepository cou;
+	@Autowired
+	VideoRepository vr;
+
+	@Autowired
+	UserRepo ur;
+
+	@Override
+	public List<Category> getAllCategory() {
+		return cat.findAll();
+	}
+
+	@Override
+	public Optional<Category> getCategoryById(int id) {
+		return cat.findById(id);
+	}
+
+	@Override
+	public void deleteCategory(int id) {
+		cat.deleteById(id);
+
+	}
+
+	@Override
+	public List<Course> getAllCourse() {
+		return cou.findAll();
+	}
+
+	@Override
+	public Optional<Course> getCourseById(int id) {
+		return cou.findById(id);
+	}
+
+	@Override
+	public void deleteCourse(int i) {
+		cou.deleteById(i);
+
+	}
+
+	@Override
+	public List<Video> getAllVideo() {
+		return vr.findAll();
+	}
+
+	@Override
+	public Optional<Video> getVideoById(int id) {
+		return vr.findById(id);
+	}
+
+	@Override
+	public void deleteVideo(int i) {
+		vr.deleteById(i);
+
+	}
 
 }
