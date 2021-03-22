e6e2c1f7b4cd26c8aebba11d18a660c29c1c96cf
Aditkumar Patel <aditkumarp@cybage.com>
2021-03-22 17:47:41 +0530

    folder structure

--- backend/src/main/java/com/elearning/controller/UserController.java
+++ backend/src/main/java/com/elearning/controller/UserController.java
@@ -1,11 +1,17 @@
-package com.example.controller;
+package com.elearning.controller;
 
+import java.io.IOException;
+import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,18 +21,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.example.service.AdminServiceImpl;
+import com.elearning.service.UserService;
+import com.elearning.service.UserServiceImpl;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.itextpdf.text.pdf.PdfStructTreeController.returnType;
 
+import io.micrometer.core.ipc.http.HttpSender.Request;
 
 @RestController
-@CrossOrigin
-public class AdminController {
+@RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+public class UserController {
+
 	
-	
+
 
 }
--- backend/src/main/java/com/elearning/service/UserService.java
+++ backend/src/main/java/com/elearning/service/UserService.java
@@ -1,4 +1,4 @@
-package com.example.service;
+package com.elearning.service;
 
 import java.io.IOException;
 import java.sql.Date;
